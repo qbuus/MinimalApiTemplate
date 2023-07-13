5009f5751116be0176cef0700c96a7a5bc496150
qbuus <jkocz867@gmail.com>
2023-07-13 21:31:07 +0200

    db migration and update-database

--- MinimalApi/Program.cs
+++ MinimalApi/Program.cs
@@ -1,5 +1,18 @@
+using Application.Abstractions;
+using DataAccess;
+using DataAccess.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var cs = builder.Configuration.GetConnectionString("Default");
+
+builder.Services.AddDbContext<SocialDbContext>(opt => opt.UseSqlServer(cs));
+builder.Services.AddScoped<IPostsRepository, PostRepository>();
+
 var app = builder.Build();
+
 app.UseHttpsRedirection();
+
 app.Run();
 
