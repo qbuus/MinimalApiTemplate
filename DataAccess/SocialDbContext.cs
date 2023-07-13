fe9c0c0d9b1fcb8b23f4787ed127ff0f439ea4f1
qbuus <jkocz867@gmail.com>
2023-07-13 20:44:20 +0200

    DbContext layer

---
+++ DataAccess/SocialDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess
+{
+    public class SocialDbContext : DbContext
+    {
+        public SocialDbContext(DbContextOptions opt) : base(opt)
+        {
+
+        }
+
+        public DbSet<Post> Posts { get; set; }
+    }
+}
