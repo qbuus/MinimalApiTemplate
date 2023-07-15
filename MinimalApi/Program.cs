3bfe9b1b81684ed732a085a160015aa5935001a7
qbuus <jkocz867@gmail.com>
2023-07-15 12:58:58 +0200

    mediatR services

--- MinimalApi/Program.cs
+++ MinimalApi/Program.cs
@@ -1,7 +1,10 @@
 using Application.Abstractions;
+using Application.Posts.Commands;
 using DataAccess;
 using DataAccess.Repositories;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MediatR;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,10 +12,15 @@ var cs = builder.Configuration.GetConnectionString("Default");
 
 builder.Services.AddDbContext<SocialDbContext>(opt => opt.UseSqlServer(cs));
 builder.Services.AddScoped<IPostsRepository, PostRepository>();
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining(typeof(CreatePost)));
 
 var app = builder.Build();
 
+// middleware area
+
 app.UseHttpsRedirection();
+
+// route area
 
 app.Run();
 
