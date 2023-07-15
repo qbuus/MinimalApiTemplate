3f0d6965f1409d5db1a2ff7db521e4734991b1c6
qbuus <jkocz867@gmail.com>
2023-07-15 13:59:13 +0200

    route group

--- MinimalApi/EndpointDefinitions/PostEndpointDefinition.cs
+++ MinimalApi/EndpointDefinitions/PostEndpointDefinition.cs
@@ -10,35 +10,37 @@ namespace MinimalApi.EndpointDefinitions
     {
         public void RegisterEndpoints (WebApplication app) 
         {
-            app.MapGet("/api/posts/{id}", async (IMediator mediator, int id) =>
+            var posts = app.MapGroup("/api/posts");
+
+            posts.MapGet("/{id}", async (IMediator mediator, int id) =>
             {
                 var getPost = new GetPostById { Id = id };
                 var post = await mediator.Send(getPost);
                 return Results.Ok(post);
             }).WithName("GetPostById");
 
-            app.MapPost("/api/posts", async (IMediator mediator, Post post) =>
+            posts.MapPost("/", async (IMediator mediator, Post post) =>
             {
                 var createPost = new CreatePost { PostContent = post.Content };
                 var createdPost = await mediator.Send(createPost);
                 return Results.CreatedAtRoute("GetPostById", new { createdPost.Id }, createdPost);
             });
 
-            app.MapGet("api/posts", async (IMediator mediator) =>
+            posts.MapGet("/", async (IMediator mediator) =>
             {
                 var postsCommand = new GetAllPosts();
                 var posts = await mediator.Send(postsCommand);
                 return Results.Ok(posts);
             });
 
-            app.MapPut("api/post/{id}", async (IMediator mediator, Post post, int id) =>
+            posts.MapPut("/{id}", async (IMediator mediator, Post post, int id) =>
             {
                 var updatePost = new UpdatePost { Id = id, Content = post.Content };
                 var updatedPost = await mediator.Send(updatePost);
                 return Results.Ok(updatedPost);
             });
 
-            app.MapDelete("/api/posts/{id}", async (IMediator mediator, int id) =>
+            posts.MapDelete("/{id}", async (IMediator mediator, int id) =>
             {
                 var deletePostById = new DeletePost { Id = id };
                 await mediator.Send(deletePostById);
